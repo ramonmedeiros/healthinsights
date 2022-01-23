9519f7ace74a42477ec3351ddff3790ae0dfe57e
Ramon Medeiros <ramon.medeiros@voiapp.io>
2022-01-23 17:22:10 +0100

    Working with zoom on Heartbeat

--- src/Heartbeat.tsx
+++ src/Heartbeat.tsx
@@ -4,11 +4,6 @@ import { db } from './db'
 import { HEARTBEAT_SERIES } from "./manipulations"
 import ReactECharts from 'echarts-for-react';
 
-const upColor = '#ec0000';
-const downColor = '#00da3c';
-
-const upBorderColor = '#8A0000';
-const downBorderColor = '#008F28';
 
 export default function HeartBeat() {
     const [option, setOption] = useState<{}>({})
@@ -20,20 +15,71 @@ export default function HeartBeat() {
             const options = {
                 dataset: {
                     source: s,
-                    dimensions: ['creationDate', 'value', 'startDate', 'endDate']
-                  },
+                    dimensions: [{name: 'creationDate'}, 
+                                 {name: 'value', type: 'number', displayName: 'BPM'}, 
+                                 {name: 'startDate'}, 
+                                 {name: 'endDate'}]
+                },
                 xAxis: {
-                      type: 'category',
+                    type: 'category',
+                    axisLabel: {
+                        formatter: function (dateString: string) {
+                            let date = new Date(dateString)
+                            return `${date.toLocaleString('en-gb', {
+                                year: "numeric",
+                                month: "numeric",
+                                day: "numeric",
+                                hour: "numeric",
+                                minute: "numeric",
+                            })}`
+                        },
                     },
-                    
-                yAxis: {},
+                    axisLine: { onZero: false },
+                    axisTick: { show: false },
+                    splitLine: { show: false },
+                },
+                yAxis: {
+                    axisLabel: {
+                        formatter: `{value} BPM`,
+                    },
+                },
                 series: [
                     {
                         type: 'line',
                         smooth: true
                     },
                 ],
-                tooltip:{},
+                tooltip: {
+                    trigger: 'axis',
+                    axisPointer: {
+                        type: 'line',
+                        label: {
+                            formatter: function (props: { value: string, axisDimension: string, axisIndex: number, seriesData: any }) {
+                                let date = new Date(props.value)
+                                return `BPM on ${date.toLocaleString('en-gb', {
+                                    year: "numeric",
+                                    month: "numeric",
+                                    day: "numeric",
+                                    hour: "numeric",
+                                    minute: "numeric",
+                                })}`
+                            },
+                        }, 
+                    },
+                },
+                toolbox: {
+                    feature: {
+                        dataZoom: {
+                            yAxisIndex: false
+                        }
+                    }
+                },
+                dataZoom: [
+                    {
+                        type: 'inside',
+                        throttle: 50
+                    },
+                ],
             }
             setOption(options)
         })
